2a18909130cff77bbaa6a09b81c90ed86f501b72
adanikel@gmail.com <adanikel@gmail.com>
2021-07-21 19:58:16 +0300

    add selection sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "utils.cpp"
+
+template <typename T>
+void selection_sort(T* array_ptr, unsigned int array_len)
+{
+	T min_val{ array_ptr[0] };
+	for (unsigned int idx = 0; idx < array_len; idx++)
+	{
+	
+	}
+}
