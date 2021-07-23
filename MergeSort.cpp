637a329fd73d5ad79a6e02e8c51bdae22d6d9f52
adanikel <adanikel@gmail.com>
2021-07-23 13:49:00 +0300

    fix mergesort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -7,7 +7,7 @@ void merge(T* array_ptr, unsigned int first_idx, unsigned int second_idx, unsign
 {
 	// this method iterates over both the sub arrays and creates a new sorted one. Important to pass in this method indices of two already sorted arrays
 	unsigned int len_first = second_idx - first_idx; 
-	unsigned int len_second = array_len - second_idx; 
+	unsigned int len_second = array_len - len_first; 
 
 	T first_arr[len_first];
 	T second_arr[len_second];
@@ -24,7 +24,7 @@ void merge(T* array_ptr, unsigned int first_idx, unsigned int second_idx, unsign
 	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int k = first_idx; // because we start merging into the beginning of the first array
-
+	
 	while (i < len_first && j < len_second) // while any elements are left in both of the arrays, afterwards insert remainder
 	{
 		if (first_arr[i] > second_arr[j]) // if array two is smaller
@@ -39,7 +39,7 @@ void merge(T* array_ptr, unsigned int first_idx, unsigned int second_idx, unsign
 		}
 		k++; // after insertion, increment iteration over the sorted part of the original array
 	}
-
+	
 	if (i < len_first) // remaining into array
 	{
 		array_ptr[k] = first_arr[i];
@@ -57,24 +57,21 @@ void merge(T* array_ptr, unsigned int first_idx, unsigned int second_idx, unsign
 template <typename T>
 void merge_sort(T* array_ptr, unsigned int array_len, unsigned int start_idx = 0)
 {
-        unsigned int div_idx = std::floor(array_len / 2);
-       	std::cout << div_idx + start_idx << std::endl;
-	 if (div_idx > 1)
-        {
-                merge_sort<T>(array_ptr, start_idx + div_idx, start_idx);
-                merge_sort<T>(array_ptr, start_idx + array_len - div_idx, start_idx + div_idx + 1);
-        }
-        else
-        {
-                merge<T>(array_ptr, start_idx, start_idx + div_idx, array_len);
-        }
+	unsigned int div_idx = std::floor(array_len / 2); // divide array into two subarrays
+	if (div_idx > 1)
+	{	
+        	merge_sort<T>(array_ptr, div_idx, start_idx); // recursively sort the first subarray
+        	merge_sort<T>(array_ptr, array_len - div_idx, start_idx + div_idx); // recursively sort the second subarray
+        
+        	merge<T>(array_ptr, start_idx, start_idx + div_idx, array_len); // merge the two subarrays into one sorted array
+	}
 }
 
 int main()
 {
 	int arr[] = { -1, 6, 5, 9, 3, 7, 1, 0, 10 };
 	merge_sort<int>(arr, 9);
-	print_array<int>(arr, 3);
+	print_array<int>(arr, 9);
 	
 	return 0;
 }
